f7de2e5890b46a1328384f3165d1eade76a17d4b
jackiejoe45 <ben.abdul@ashesi.edu.gh>
2023-03-17 09:47:31 +0000

    First Version. Comments to be added soon

--- src/Clock.java
+++ src/Clock.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 public class Clock {
     private int seconds;
     private int minutes;
@@ -11,6 +12,10 @@ public class Clock {
     public static int MINUTESINHOUR = 60;
     public static int MINUTESINDAY= 1440;
     public static int HOURSINDAY=24;
+    public static int HOUR12CLOCK= 12;
+    public static int HOUR24CLOCK=24;
+
+    private boolean is24HourClock = false;
 
     public Clock(){
         this.seconds = 0;
@@ -21,23 +26,29 @@ public class Clock {
         this.seconds+= seconds % SECONDSINMINUTE;
         this.minutes+= seconds / SECONDSINMINUTE;
         this.hours+= seconds / SECONDSINHOUR;
+        this.dayOfMonth+= seconds /SECONDSINDAY;
 
         this.minutes+= minutes % MINUTESINHOUR;
         this.hours+= minutes/MINUTESINHOUR;
+        this.dayOfMonth+= minutes/MINUTESINDAY;
 
-        this.hours += hours;
+        this.hours += hours % HOURSINDAY;
+        this.dayOfMonth += hours/HOURSINDAY;
 
     }
     public Clock(double seconds,double minutes,double hours){
         this.seconds += seconds % SECONDSINMINUTE;
         this.minutes+= seconds / SECONDSINMINUTE;
         this.hours+= seconds / SECONDSINHOUR;
+        this.dayOfMonth+= seconds /SECONDSINDAY;
 
+        this.dayOfMonth+= minutes/MINUTESINDAY;
         this.minutes += (int)minutes % MINUTESINHOUR;
         this.hours+= (int)minutes/MINUTESINHOUR;
         double secs = (minutes - (int)minutes);
         this.seconds = (int) (SECONDSINMINUTE * secs);
 
+        this.dayOfMonth += hours/HOURSINDAY;
         this.hours+=hours % MINUTESINHOUR;
         double mins = (hours - (int)hours);
         this.minutes = (int) (MINUTESINHOUR * mins);
@@ -63,6 +74,14 @@ public class Clock {
     }
     public int getYear() {
         return year;
+    }
+
+    public boolean isIs24HourClock() {
+        return is24HourClock;
+    }
+
+    public void setIs24HourClock(boolean is24HourClock) {
+        this.is24HourClock = is24HourClock;
     }
 
     public void setSeconds(int seconds) {
@@ -117,9 +136,24 @@ public class Clock {
 
     }
     public String getTime(){
+        checkHours();
+        DecimalFormat twoDigitFormat = new DecimalFormat("00");
         return "Current Time is (HH:MM:SS) - "
-                + getHours()+":"
-                + getMinutes()+":"
-                + getSeconds();
+                + twoDigitFormat.format(getHours())+":"
+                + twoDigitFormat.format(getMinutes())+":"
+                + twoDigitFormat.format(getSeconds());
     }
+    public void checkHours(){
+        if(is24HourClock == true)
+        {
+            this.dayOfMonth+=this.hours / HOUR24CLOCK;
+            this.hours = this.hours % HOUR24CLOCK;
+        }
+        else
+        {
+            this.dayOfMonth+=this.hours / HOUR12CLOCK;
+            this.hours = this.hours % HOUR12CLOCK;
+        }
+    }
+
 }
--- src/TestClock.java
+++ src/TestClock.java
@@ -1,17 +1,12 @@
 public class TestClock {
     public static void main(String[] args){
-        Clock digital = new Clock();
-        Clock analog1 = new Clock(900,600.65,34.5);
-        Clock analog2 = new Clock(900,600,34);
+        Clock digital = new Clock(901,600,34);
+        Clock analog1 = new Clock(920,600.65,37.5);
+        Clock analog2 = new Clock(920,600.65,37.5);
+        analog1.setIs24HourClock(true);
+        analog2.setIs24HourClock(false);
         System.out.println(digital.getTime());
         System.out.println(analog1.getTime());
         System.out.println(analog2.getTime());
-        System.out.println(digital.getSeconds());
-        System.out.println(digital.getMinutes());
-        System.out.println(digital.getHours());
-        System.out.println(digital.getDayOfMonth());
-        System.out.println(digital.getMonth());
-        System.out.println(digital.getYear());
-
     }
 }
